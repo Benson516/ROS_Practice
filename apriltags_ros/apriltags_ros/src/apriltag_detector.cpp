c57b8d9f47c4f22c162742db676dd157159fe3d4
Benson516 <benson516@hotmail.com>
2017-10-11 21:43:22 +0800

    Fix bugs about sweeping ROI and speed calculation

--- apriltags_ros/apriltags_ros/src/apriltag_detector.cpp
+++ apriltags_ros/apriltags_ros/src/apriltag_detector.cpp
@@ -112,8 +112,8 @@ cv::Rect convert_centerPoint_to_ROI(int Cx, int Cy, int width, int height, int x
 }
 // Scan through the entire image
 // Upperbound indexes to the point that is not included.
-void MoveTheROI(int &Cx, int &Cy, int &width, int &height, int x_upperbound, int y_upperbound, int x_lowerBound=0, int y_lowerBound=0, int step_x=0, int step_y=0){
-    //
+void MoveTheROI(int &Cx, int &Cy, int &width, int &height, int x_upperbound, int y_upperbound, int x_lowerBound=0, int y_lowerBound=0, int step_x=0, int step_y=0, int min_pixel=20){
+    // Default step: no overlapping between windows
     if (step_x <= 0){
       step_x = width;
     }
@@ -127,12 +127,13 @@ void MoveTheROI(int &Cx, int &Cy, int &width, int &height, int x_upperbound, int
     int x_L = (Cx - half_width) + step_x;
     int y_L = Cy - half_height;
     //
-    if (x_L >= x_upperbound){
+    // int min_pixel = 20;
+    if (x_L >= (x_upperbound - min_pixel)){
         // To the next line
         x_L = x_lowerBound;
         y_L += step_y;
         //
-        if (y_L >= y_upperbound){
+        if (y_L >= (y_upperbound - min_pixel)){
             // To the left-top corner
             y_L = y_lowerBound;
         }
@@ -225,7 +226,7 @@ void AprilTagDetector::imageCb(const sensor_msgs::ImageConstPtr& msg,const senso
     ROI_width_set = nCol;
     */
 
-    /*    
+    /*
     //
     ROI_height_set = nRow/2 - y_border;
     ROI_width_set = nCol/2 - x_border;
@@ -268,22 +269,22 @@ void AprilTagDetector::imageCb(const sensor_msgs::ImageConstPtr& msg,const senso
     }
     */
 
-    
+
     //
     // ROI_width_set = (nCol - 2*x_border)/3;
     // ROI_height_set = (nRow - 2*y_border)/3;
     ROI_width_set = ROI_width/2*3;
     ROI_height_set = ROI_height/2*3;
 
-    //   
+    //
     // Move the ROI by z-type scan sequnce
     MoveTheROI(Cx, Cy, ROI_width_set, ROI_height_set, (nCol-x_border), (nRow-y_border), x_border, y_border, (ROI_width_set), (ROI_height_set));
     // MoveTheROI(Cx, Cy, ROI_width_set, ROI_height_set, (nCol-x_border), (nRow-y_border), x_border, y_border, (ROI_width_set)/2, (ROI_height_set)/2);
-    
+
 
     // Reset the speed
-    speed_x = 0;
-    speed_y = 0;
+    speed_x = 0.0;
+    speed_y = 0.0;
   }else{
     //
     if (detections.size() == 0){
@@ -302,27 +303,41 @@ void AprilTagDetector::imageCb(const sensor_msgs::ImageConstPtr& msg,const senso
       int Cy_last = roi_rect.y + detections[0].cxy.second/resize_scale;
       //
       if(count_tag_loss != 0){
+        // The first time
         speed_x = 0.0;
         speed_y = 0.0;
-      }else{ 
-        //
+      }else{ // At least the second time
+
+        // For overcomeing the initial transient
         if (speed_x == 0.0){
             speed_x = double(Cx_last - Cx);
         }else{
-            speed_x += speed_filterRatio*(double(Cx_last - Cx) - speed_x);
+            speed_x += speed_filterRatio*( double(Cx_last - Cx) );
         }
         //
         if (speed_y == 0.0){
             speed_y = double(Cy_last - Cy);
         }else{
-            speed_y += speed_filterRatio*(double(Cy_last - Cy) - speed_y);
+            speed_y += speed_filterRatio*( double(Cy_last - Cy) );
         }
-        //
+
+        // TODO Note: The following two lines are only true when the previous speeds are both zero
         // speed_x = double(Cx_last - Cx);
         // speed_y = double(Cy_last - Cy);
-        //
+
+        // TODO Note: the following two lines are wrong implementations.
         // speed_x += speed_filterRatio*(double(Cx_last - Cx) - speed_x);
         // speed_y += speed_filterRatio*(double(Cy_last - Cy) - speed_y);
+
+        /*
+        // TODO Note: The following are correct implementations.
+        // The controller for speed estimation
+        // The error is (the real position - the estimated position)
+        // Cx_last - Cx = (Cx_last - Cx_last_last) - speed_x_last
+        // --> This is actully a low pass filter for speed.
+        speed_x += speed_filterRatio*( double(Cx_last - Cx) );
+        speed_y += speed_filterRatio*( double(Cy_last - Cy) );
+        */
       }
       //
       Cx = Cx_last + int(speed_x);
